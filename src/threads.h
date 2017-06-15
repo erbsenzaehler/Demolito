0ffeeb6a7eb913d2a17b5e8e1ff84ed03181037c
lucasart <lucas.braesch@gmail.com>
2017-06-15 11:22:17 +0800

    Use WIN32 API's CriticalSection

--- src/threads.h
+++ src/threads.h
@@ -4,7 +4,6 @@
 #include <pthread.h>
 
 #define thread_local _Thread_local
-typedef pthread_mutex_t mtx_t;
 typedef pthread_t thrd_t;
 typedef int (*thrd_start_t)(void*);
 
@@ -12,10 +11,27 @@ enum {mtx_plain/*, mtx_recursive, mtx_timed*/};
 enum {thrd_success/*, thrd_timedout, thrd_busy, thrd_error, thrd_nomem*/};
 
 /* Return thrd_success if ok */
+#ifdef _WIN64
+// Win32 API Critical Section (workaround mingw bug)
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+typedef CRITICAL_SECTION mtx_t;
+
+#define mtx_init(m, t) InitializeCriticalSection(m)
+#define mtx_destroy(m) DeleteCriticalSection(m)
+#define mtx_lock(m) EnterCriticalSection(m)
+#define mtx_unlock(m) LeaveCriticalSection(m)
+#else
+// POSIX mutex
+typedef pthread_mutex_t mtx_t;
+
 #define mtx_init(m, t) pthread_mutex_init(m, NULL)
 #define mtx_destroy(m) pthread_mutex_destroy(m)
 #define mtx_lock(m) pthread_mutex_lock(m)
 #define mtx_unlock(m) pthread_mutex_unlock(m)
+#endif
 
 #define thrd_create(thrd, func, args) pthread_create(thrd, 0, (void*(*)(void*))func, args)
 #define thrd_join(thrd, dummy) pthread_join(thrd, NULL)
